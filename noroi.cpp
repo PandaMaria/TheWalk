5f1b0c556edceb507121d87fa028bbd86472b50d
PandaMaria <61808242+PandaMaria@users.noreply.github.com>
2020-05-04 00:06:18 +0300

    Update noroi.cpp

--- noroi.cpp
+++ noroi.cpp
@@ -1,13 +1,9 @@
-#include<iostream>
-#include"leu.h"
-#include"capcana.h"
-using namespace std;
 
-void Capcana::setPozitieNoroi(const PozitieCapcana& newPozitie) {
-	max=19;
-	pozitie.rand = rand()%max;;
-	pozitie.coloana = rand()%max;;
-}
-Capcana::PozitieCapcana Capcana::getPozitieCapcana()const {
-	return pozitie;
-}
+#include"capcana.h"
+#include"lume.h"
+
+class Noroi{
+    Capcana* noroi;
+    void setPozitieNoroi()
+    int getPozitieNoroi();
+};
