179fb64d4a03c20a22517dfb4a45c7fbe207b5b3
PandaMaria <61808242+PandaMaria@users.noreply.github.com>
2020-05-04 00:05:04 +0300

    Update capcana.cpp

--- capcana.cpp
+++ capcana.cpp
@@ -1,1 +1,13 @@
 
+#include<iostream>
+#include"capcana.h"
+using namespace std;
+
+void Capcana::setPozitieCapcana(const Pozitie& newPozitie) {
+	max=19;
+	pozitie.rand = rand()%max;;
+	pozitie.coloana = rand()%max;;
+}
+Capcana::PozitieCapcana Capcana::getPozitieCapcana()const {
+	return pozitie;
+}
